3e94b9cfef5496035da7b8797882abb67f673244
klozz <klozz@vivaldi.net>
2019-09-30 05:41:24 +0000

    data-ipa-cfg-mgr: Use project pathmap
    Change-Id: I9338194d560f62e09d5f331100bc66061bd7b8a6
    Signed-off-by: klozz <klozz@vivaldi.net>

--- ipacm_vendor_product.mk
+++ ipacm_vendor_product.mk
@@ -1,6 +1,14 @@
+ifeq ($(call my-dir),$(call project-path-for,qcom-data-ipa-cfg-mgr))
+
+LOCAL_PATH := $(call my-dir)
+
+include $(call first-makefiles-under,$(LOCAL_PATH))
+
 #IPACM_DATA
 IPACM_DATA += IPACM_cfg.xml
 IPACM_DATA += ipacm
 IPACM_DATA += ipacm.rc
 
 PRODUCT_PACKAGES += $(IPACM_DATA)
+
+endif
